52da46e33e7ad44c7525d2ca28b4fc6f38e4ab46
quadrel-design <147716606+quadrel-design@users.noreply.github.com>
2025-04-02 10:31:17 +0200

    Update and rename next.config.js to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
-const withNextra = require('nextra')({
+// next.config.mjs
+
+import nextra from 'nextra'
+
+const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
 
 const repo = 'docs'
 
-module.exports = {
-  ...withNextra(),
+export default {
+  ...withNextra,
   output: 'export',
   basePath: `/${repo}`,
   assetPrefix: `/${repo}/`,
