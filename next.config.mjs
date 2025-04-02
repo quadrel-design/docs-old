e77c661fa55aa0a511f2508436b9797f8a4464db
quadrel-design <147716606+quadrel-design@users.noreply.github.com>
2025-04-02 15:56:46 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
+/** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/docs/' : '',
-  basePath: isProd ? '/docs' : '',
-  output: 'export'
+  assetPrefix: isProd ? '/docs/' : '',  // Hier wird der Repository-Name eingebaut
+  basePath: isProd ? '/docs' : '',      // Hier wird der Repository-Name eingebaut
+  output: 'export',  // Aktiviert den statischen Export
+  trailingSlash: true  // Fügt einen Schrägstrich am Ende der URLs hinzu (nützlich für GitHub Pages)
 };
 
 export default nextConfig;
