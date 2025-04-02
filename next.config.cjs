8a8a820d40e34207e9aea1a23d00a61c998398d3
quadrel-design <147716606+quadrel-design@users.noreply.github.com>
2025-04-02 13:34:59 +0200

    Update next.config.cjs

--- next.config.cjs
+++ next.config.cjs
@@ -2,7 +2,6 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
-
 module.exports = withNextra({
   output: 'export',
   trailingSlash: true
