811ac29422736dcdd1c3d59c36782497ba37e5c0
quadrel-design <147716606+quadrel-design@users.noreply.github.com>
2025-04-02 13:35:57 +0200

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,19 +1,8 @@
-import type { DocsThemeConfig } from 'nextra-theme-docs'
-
-const config: DocsThemeConfig = {
-  logo: <span>Quadrel Docs</span>,
+export default {
   project: {
     link: 'https://github.com/quadrel-design/docs',
   },
   docsRepositoryBase: 'https://github.com/quadrel-design/docs/blob/main',
-  footer: {
-    text: '© 2025 Quadrel Docs',
-  },
-  useNextSeoProps() {
-    return {
-      titleTemplate: '%s – Quadrel Docs',
-    }
-  }
+  titleSuffix: ' – Quadrel Docs',
+  logo: <span>Quadrel Docs</span>,
 }
-
-export default config
